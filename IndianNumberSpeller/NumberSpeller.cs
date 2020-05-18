e8e002c02ae061fad8708a0015487fcbb3703ff9
Veera Nagendra Babu Bandarulanka <vbandarulank@dxc.com>
2020-05-18 21:25:06 +0530

    Added Initial Solution

---
+++ IndianNumberSpeller/NumberSpeller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IndianNumberSpeller
+{
+    public class NumberSpeller
+    {
+        public NumberSpeller()
+        {
+        }
+
+        public string SpellANumber(int input)
+        {
+            if (input < 0)
+                throw new ArgumentException("Given Number should be a whole number");
+
+            return string.Empty;
+        }
+    }
+}
---
+++ IndianNumberSpellerTester/IndianNumberSpellerUnitTest.cs
@@ -0,0 +1,32 @@
+﻿using IndianNumberSpeller;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndianNumberSpellerTester
+{
+    [TestFixture]
+    public class IndianNumberSpellerUnitTest
+    {
+        NumberSpeller numberSpeller = new NumberSpeller();
+        [Test]
+        public void DoNothing()
+        {
+            Assert.IsTrue(true);
+        }
+        [Test]
+        public void CheckIfaNumberIsWholeNumberOrNot()
+        {
+            Assert.Throws<ArgumentException>(() => numberSpeller.SpellANumber(-1));
+        }
+
+        [Test]
+        public void CheckIfBasicresponseIsReturned()
+        {
+            string expectedResult = "zero";
+            var result = numberSpeller.SpellANumber(0);
+            Assert.AreNotEqual(expectedResult, result);
+        }
+    }
+}
